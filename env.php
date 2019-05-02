b4c831934ad0c008760291905f6954db4e3cd67c
MagenX <magenx@users.noreply.github.com>
2019-05-02 13:05:14 +0100

    Create env.php

---
+++ env.php
@@ -0,0 +1,64 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'XXX'
+    ],
+    'crypt' => [
+        'key' => 'XXX'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'XXX',
+                'username' => 'XXX',
+                'password' => 'XXX',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'cge_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'cge_'
+            ]
+        ]
+    ],
+    'cache_types' => [
+        'config' => 0,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 0,
+        'reflection' => 0,
+        'db_ddl' => 0,
+        'compiled_config' => 0,
+        'eav' => 0,
+        'customer_notification' => 0,
+        'config_integration' => 0,
+        'config_integration_api' => 0,
+        'full_page' => 0,
+        'config_webservice' => 0,
+        'translate' => 0,
+        'vertex' => 0
+    ],
+    'install' => [
+        'date' => 'Sat, 27 Apr 2019 09:12:48 +0000'
+    ]
+];
