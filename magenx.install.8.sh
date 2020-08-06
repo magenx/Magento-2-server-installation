8d136b1f4522a26cf05eb79b2d7120666632b1a7
MagenX <magenx@users.noreply.github.com>
2020-08-06 15:32:22 +0100

    Update magenx.install.8.sh

--- magenx.install.8.sh
+++ magenx.install.8.sh
@@ -1474,7 +1474,7 @@ echo
 GREENTXT "REDIS CACHE AND SESSION STORAGE"
 echo
 systemctl start redis.target
-redis-cli -p6380 config set save ""
+redis-cli -p 6380 config set save ""
 ## cache backend
 cd ${MAGE_WEB_ROOT_PATH}
 su ${MAGE_OWNER} -s /bin/bash -c "bin/magento setup:config:set \
