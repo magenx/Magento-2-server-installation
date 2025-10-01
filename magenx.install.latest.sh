b4b0b9ba3ebefa7bcbbd4b4bd34b1dc0a12fda8c
Scott Wakefield <scott@npclinics.com.au>
2025-10-02 07:45:37 +0800

    Update the install script to install plocate instead of mlocate.
    The mlocate package has been removed from Debian 13 repositories.
    
    It has been replaced by plocate, which provides the same locate and updatedb commands.

--- magenx.install.latest.sh
+++ magenx.install.latest.sh
@@ -50,7 +50,7 @@ MALDET="https://www.rfxn.com/downloads/maldetect-current.tar.gz"
 WEB_STACK_CHECK="mysql* rabbitmq* elasticsearch opensearch percona-server* maria* php* nginx* varnish* certbot* redis* webmin"
 
 EXTRA_PACKAGES="curl jq gnupg2 auditd apt-transport-https apt-show-versions ca-certificates lsb-release make autoconf snapd automake libtool uuid-runtime \
-perl openssl unzip screen nfs-common inotify-tools iptables smartmontools mlocate vim wget sudo apache2-utils python3-setuptools \
+perl openssl unzip screen nfs-common inotify-tools iptables smartmontools plocate vim wget sudo apache2-utils python3-setuptools \
 logrotate git netcat-openbsd patch ipset postfix strace rsyslog moreutils lsof sysstat acl attr iotop expect imagemagick snmp ssl-cert-check ufw gettext-base"
 
 PERL_MODULES="liblwp-protocol-https-perl libdbi-perl libconfig-inifiles-perl libdbd-mysql-perl libterm-readkey-perl"
