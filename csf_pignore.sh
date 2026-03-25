6eef76c8f9e4b249da7c7fc149c44ca8fc8a4485
MagenX <magenx@users.noreply.github.com>
2026-03-25 15:11:48 +0100

    Delete csf_pignore.sh

--- csf_pignore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## disable noisy firewall alerts
-for program in $(grep -o 'EXE:.*' /var/log/lfd.log | awk -F' ' '{print tolower($1)}' | sort -u)
-do 
-echo "${program}" >> /etc/csf/csf.pignore
-done
-
-csf -ra
